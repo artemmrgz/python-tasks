aa4fc83f8ef68a4f121b0e39e87cd759e6342bd9
Artem Marhaza <artemmrgz@gmail.com>
2021-09-16 12:23:25 +0200

    changed chessboard

--- tasks/chessboard.py
+++ tasks/chessboard.py
@@ -7,13 +7,17 @@ class Chessboard:
         self.width = self.__check_value(width)
 
     def __str__(self):
-        return self.__create_board()
+        board = self.__create_board()
+        rows_to_str = [''.join(rows) for rows in board]
+        return '\n'.join(rows_to_str)
+
 
     @staticmethod
     def __check_value(value):
-        if isinstance(value, int) and value >= 0:
+        if value > 0:
             return value
         raise ValueError('Incorrect value. Value should be non-negative integer')
+
 
     def __create_board(self):
         board = [['*' for i in range(self.width)] for j in range(self.height)]
@@ -21,21 +25,19 @@ class Chessboard:
             for j in range(len(board[i])):
                 if (i % 2 == 0 and j % 2 == 1) or (i % 2 == 1 and j % 2 == 0):
                     board[i][j] = ' '
-        board = [''.join(i) for i in board]
-        result = '\n'.join(board)
-        return result
+        return board
 
 
 def create_parser():
     parser = argparse.ArgumentParser(description='Program draws a chessboard with height and width provided by user')
-    parser.add_argument('height', help='height of the chessboard')
-    parser.add_argument('width', help='width of the chessboard')
+    parser.add_argument('height', type=int, help='height of the chessboard')
+    parser.add_argument('width', type=int, help='width of the chessboard')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
+    args = create_parser()
     try:
-        args = create_parser()
         board = Chessboard(args.height, args.width)
         print(board)
     except ValueError as e:
